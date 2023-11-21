6e1b56d0289892c0719ed145475aab831dbd98c7
Miłosz Ławacz <miloszl2000@outlook.com>
2023-11-21 18:18:57 +0100

    - added draws count, - changed the look of reset wins button - reset wins button now resets wins and starts a new game

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let turn, square, isChecked, crossWins=0, circleWins=0;
+let turn, square, isChecked, crossWins=0, circleWins=0, draws=0, checkedSquares;
 const turnRemind = document.getElementById('turn');
 const squares = document.getElementsByClassName('square');
 
@@ -16,50 +16,51 @@ function startGame(){
         squares[i].setAttribute('checked', 'empty');
         squares[i].children[0].src = '';
     }
+
+    checkedSquares = 0;
 }
 
 function win(winner){
     document.getElementById('hover').style.display = 'block';
     document.getElementById('popup').style.display = 'flex';
-    document.querySelector('h3').innerHTML = `${winner} won!`;
-    winner==='circle' ? circleWins++ : crossWins++;
-    document.getElementById('wins').children[0].innerHTML = `Circle wins: ${circleWins}`;
-    document.getElementById('wins').children[1].innerHTML = `Cross wins: ${crossWins}`;
+    if (winner==='draw'){
+        document.querySelector('h3').innerHTML = `Draw!`;
+        draws++;
+        document.getElementById('wins').children[2].innerHTML = `Draws: ${draws}`;
+    } else {
+        document.querySelector('h3').innerHTML = `${winner} won!`;
+        winner==='Circle' ? circleWins++ : crossWins++;
+        document.getElementById('wins').children[0].innerHTML = `Circle wins: ${circleWins}`;
+        document.getElementById('wins').children[1].innerHTML = `Cross wins: ${crossWins}`;
+    }
 }
 
 function checkWinner(){
     if(squares[0].getAttribute('checked')===squares[1].getAttribute('checked') && squares[1].getAttribute('checked')===squares[2].getAttribute('checked') && squares[0].getAttribute('checked')!=='empty'){
         win(squares[0].getAttribute('checked'));
         console.log('1')
-    }
-    if(squares[0].getAttribute('checked')===squares[3].getAttribute('checked') && squares[3].getAttribute('checked')===squares[6].getAttribute('checked') && squares[0].getAttribute('checked')!=='empty'){
+    } else if(squares[0].getAttribute('checked')===squares[3].getAttribute('checked') && squares[3].getAttribute('checked')===squares[6].getAttribute('checked') && squares[0].getAttribute('checked')!=='empty'){
         win(squares[0].getAttribute('checked'));
         console.log('2')
-    }
-    if(squares[0].getAttribute('checked')===squares[4].getAttribute('checked') && squares[4].getAttribute('checked')===squares[8].getAttribute('checked') && squares[0].getAttribute('checked')!=='empty'){
+    } else if(squares[0].getAttribute('checked')===squares[4].getAttribute('checked') && squares[4].getAttribute('checked')===squares[8].getAttribute('checked') && squares[0].getAttribute('checked')!=='empty'){
         win(squares[0].getAttribute('checked'));
         console.log('3')
-    }
-    if(squares[3].getAttribute('checked')===squares[4].getAttribute('checked') && squares[4].getAttribute('checked')===squares[5].getAttribute('checked') && squares[3].getAttribute('checked')!=='empty'){
+    } else if(squares[3].getAttribute('checked')===squares[4].getAttribute('checked') && squares[4].getAttribute('checked')===squares[5].getAttribute('checked') && squares[3].getAttribute('checked')!=='empty'){
         win(squares[3].getAttribute('checked'));
         console.log('4')
-    }
-    if(squares[6].getAttribute('checked')===squares[7].getAttribute('checked') && squares[7].getAttribute('checked')===squares[8].getAttribute('checked') && squares[6].getAttribute('checked')!=='empty'){
+    } else if(squares[6].getAttribute('checked')===squares[7].getAttribute('checked') && squares[7].getAttribute('checked')===squares[8].getAttribute('checked') && squares[6].getAttribute('checked')!=='empty'){
         win(squares[6].getAttribute('checked'));
         console.log('5')
-    }
-    if(squares[6].getAttribute('checked')===squares[4].getAttribute('checked') && squares[4].getAttribute('checked')===squares[2].getAttribute('checked') && squares[6].getAttribute('checked')!=='empty'){
+    } else if(squares[6].getAttribute('checked')===squares[4].getAttribute('checked') && squares[4].getAttribute('checked')===squares[2].getAttribute('checked') && squares[6].getAttribute('checked')!=='empty'){
         win(squares[6].getAttribute('checked'));
         console.log('6')
-    }
-    if(squares[1].getAttribute('checked')===squares[4].getAttribute('checked') && squares[4].getAttribute('checked')===squares[7].getAttribute('checked') && squares[1].getAttribute('checked')!=='empty'){
+    } else if(squares[1].getAttribute('checked')===squares[4].getAttribute('checked') && squares[4].getAttribute('checked')===squares[7].getAttribute('checked') && squares[1].getAttribute('checked')!=='empty'){
         win(squares[1].getAttribute('checked'));
         console.log('7')
-    }
-    if(squares[2].getAttribute('checked')===squares[5].getAttribute('checked') && squares[5].getAttribute('checked')===squares[8].getAttribute('checked') && squares[2].getAttribute('checked')!=='empty'){
+    } else if(squares[2].getAttribute('checked')===squares[5].getAttribute('checked') && squares[5].getAttribute('checked')===squares[8].getAttribute('checked') && squares[2].getAttribute('checked')!=='empty'){
         win(squares[2].getAttribute('checked'));
         console.log('8')
-    }
+    } else if (checkedSquares===9){win('draw')}
 }
 
 function check(id){
@@ -70,13 +71,15 @@ function check(id){
         if (turn==='circle'){
             square.children[0].src = 'circle.png';
             turn = 'cross';
-            square.setAttribute('checked', "circle");
+            square.setAttribute('checked', "Circle");
             turnRemind.children[1].src = 'cross.png';
+            checkedSquares++;
         } else {
             square.children[0].src = 'cross.png';
             turn = 'circle';
-            square.setAttribute('checked', 'cross');
+            square.setAttribute('checked', 'Cross');
             turnRemind.children[1].src = 'circle.png';
+            checkedSquares++;
         }
     }
 
@@ -86,6 +89,9 @@ function check(id){
 function resetWins(){
     circleWins = 0;
     crossWins = 0;
+    draws = 0;
     document.getElementById('wins').children[0].innerHTML = `Circle wins: ${circleWins}`;
     document.getElementById('wins').children[1].innerHTML = `Cross wins: ${crossWins}`;
+    document.getElementById('wins').children[2].innerHTML = `Draws: ${draws}`;
+    startGame();
 }
